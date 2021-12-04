d595a45a883872153a481f0693bf8f6665f996e8
Pablo Mora <pmora1990@gmail.com>
2021-12-04 17:30:37 -0600

    Replace generic functions with imports from utility package

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -1,11 +1,9 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random')
 
 const settings = {
     dimensions: [1080, 1080]
-};
-
-const degToRad = (degrees) => {
-    return degrees / 180 * Math.PI;
 };
 
 const sketch = () => {
@@ -23,11 +21,11 @@ const sketch = () => {
         const h = height * 0.1;
         let x, y;
 
-        const num = 50;
+        const num = 12;
         const radius = width * 0.3;
 
         for (let i = 0; i < num; i++) {
-            const slice = degToRad(360 / num);
+            const slice = math.degToRad(360 / num);
             const angle = slice * i;
 
             x = cx + radius * Math.sin(angle);
@@ -36,6 +34,7 @@ const sketch = () => {
             context.save();
             context.translate(x, y);
             context.rotate(-angle);
+            context.scale(random.range(1, 3), 1);
 
             context.beginPath();
             context.rect(-w * 0.5, -h * 0.5, w, h);
